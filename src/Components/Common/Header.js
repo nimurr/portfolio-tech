eca3cbbc64a6106f4494408b57218f9b9d237559
nimurr <nimurnerob404@gmail.com>
2025-10-27 22:30:34 +0600

    `Refactor Header component, update Hero component, and modify global styles and layout metadata`

--- src/Components/Common/Header.js
+++ src/Components/Common/Header.js
@@ -8,43 +8,43 @@ const Header = () => {
     const toggleMenu = () => setIsOpen(!isOpen);
 
     return (
-        <div className="sticky backdrop-blur-sm  h-0 border-b border-white/20 rounded-xl w-[70%] mx-auto top-0 left-0 right-0">
-            <nav className=" backdrop-blur-sm  h-0 border-b border-white/20 rounded-xl">
+        <div className="containerCustom sticky backdrop-blur-xl z-[999] border border-gray-200 rounded-xl  mx-auto top-10 left-0 right-0">
+            <nav className="">
                 <div className=" flex justify-between items-center px-6 py-4">
                     {/* Logo */}
-                    <div className="text-2xl max-w-[100px] font-bold text-gray-800">
+                    <a href="#hero" className="text-2xl max-w-[100px] font-bold text-gray-800">
                         <img className="w-full" src="/Images/logo.png" alt="" />
-                    </div>
+                    </a>
 
                     {/* Desktop Menu */}
-                    <ul className="hidden md:flex items-center space-x-8 text-gray-800 font-medium">
+                    <ul className="hidden lg:flex items-center space-x-8 font-medium text-gray-500 text-xl">
                         <li>
-                            <a href="#features" className="hover:text-blue-600 duration-200">
+                            <a href="#features" className="hover:text-[#1b0c25] duration-200">
                                 Features
                             </a>
                         </li>
                         <li>
-                            <a href="#about" className="hover:text-blue-600 duration-200">
+                            <a href="#about" className="hover:text-[#1b0c25] duration-200">
                                 About
                             </a>
                         </li>
                         <li>
-                            <a href="#testimonials" className="hover:text-blue-600 duration-200">
+                            <a href="#testimonials" className="hover:text-[#1b0c25] duration-200">
                                 Testimonial
                             </a>
                         </li>
                         <li>
-                            <a href="#pricing" className="hover:text-blue-600 duration-200">
+                            <a href="#pricing" className="hover:text-[#1b0c25] duration-200">
                                 Pricing
                             </a>
                         </li>
                         <li>
-                            <a href="#faq" className="hover:text-blue-600 duration-200">
+                            <a href="#faq" className="hover:text-[#1b0c25] duration-200">
                                 FAQ
                             </a>
                         </li>
                         <li>
-                            <a href="#contact" className="hover:text-blue-600 duration-200">
+                            <a href="#contact" className=" bg-[#1b0c25] hover:bg-[#311742] py-3 px-5 rounded-md text-white duration-200">
                                 Contact
                             </a>
                         </li>
@@ -53,7 +53,7 @@ const Header = () => {
                     {/* Mobile Menu Button */}
                     <button
                         onClick={toggleMenu}
-                        className="md:hidden text-gray-800 focus:outline-none"
+                        className="lg:hidden text-gray-800 focus:outline-none"
                     >
                         {isOpen ? <FaTimes size={26} /> : <FaBars size={26} />}
                     </button>
@@ -61,8 +61,8 @@ const Header = () => {
 
                 {/* Mobile Dropdown Menu */}
                 {isOpen && (
-                    <div className="md:hidden bg-white/70 backdrop-blur-md border-t border-white/20">
-                        <ul className="flex flex-col items-center py-4 space-y-4 text-gray-800 font-medium">
+                    <div className="lg:hidden absolute top-full left-0 w-full backdrop-blur-2xl rounded-2xl border bg-[#19181aec] border-white/50 text-white">
+                        <ul className="flex flex-col items-center py-4 space-y-4  font-medium">
                             <li>
                                 <a href="#features" onClick={toggleMenu}>
                                     Features
--- src/Components/Home/Hero.js
+++ src/Components/Home/Hero.js
@@ -1,10 +1,27 @@
+'use client'
 import React from 'react';
 
 const Hero = () => {
     return (
         <section id='hero'>
-            <div className='bgImageUseBanner w-[98%] mx-auto rounded-xl overflow-hidden '>
-                <img className='w-96 h-96' src="/Images/banner.png" alt="" />
+            <div className='bgImageUseBanner w-[98%] mx-auto rounded-xl overflow-hidden flex items-end'>
+                <div className='containerCustom'>
+                    <div>
+                        <h2 className='flex border uppercase items-center border-[#ffffff25] w-fit rounded-full py-1 px-3 backdrop-blur-xl bg-[#ffffff23]'>
+                            <img className='mr-2 w-5' src="https://img.icons8.com/?size=48&id=YMqRHr9A2OZh&format=png" alt="" />
+                            business & solution
+                        </h2>
+                        <h1 className='text-8xl font-bold my-5'>The AI-powered Customer <br /> Service Platform</h1>
+                        <p className='text-xl text-[#1b0c25] my-10'>Fluence AI helps you connect, manage, and optimize your AI tools effortlessly. Unlock powerful <br />  insights and automate complex processes with ease.</p>
+                        <div>
+                            <button className='mr-5 cursor-pointer bg-[#1b0c25] text-white py-3 px-8 rounded-xl'>Get Started</button>
+                            <button className='mr-5 cursor-pointer backdrop-blur-xl bg-[#ffffff0a] border border-[#ffffff25] text-white py-3 px-8 rounded-xl'>Book a Demo</button>
+                        </div>
+                    </div>
+                    <div className='h-[60vh] w-full border-[1px] border-[#ffffff25] rounded-t-4xl mt-10 backdrop-blur-xl '>
+
+                    </div>
+                </div>
             </div>
         </section>
     );
